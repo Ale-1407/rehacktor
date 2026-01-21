b0fb5404a9229a9707e69f0cd9b3f01703487cbf
Ale-1407 <alessiacarrera07@gmail.com>
2026-01-21 09:55:39 +0100

    fix: div remove and click cards

--- src/components/HomeComponents/GameCard.jsx
+++ src/components/HomeComponents/GameCard.jsx
@@ -4,31 +4,22 @@ export default function GameCard({ game }) {
   return (
     <>
       <div className="hover-3d relative">
-        <figure className="relative overflow-hidden w-full rounded-2xl aspect-4/5 lg:aspect-video">
-          {/* content */}
-          <Link to={`/detail/${game.id}`}>
+        {/* content */}
+        <Link to={`/detail/${game.id}`}>
+          <figure className="relative overflow-hidden w-full rounded-2xl aspect-4/5 lg:aspect-video">
             <img
               src={`${game.background_image}`}
               alt=""
               className="w-full h-full object-cover"
             />
-          </Link>
-          <p
-            className="absolute bottom-0 w-full text-center bg-linear-to-t from-black/70  to-transparent
+            <p
+              className="absolute bottom-0 w-full text-center bg-linear-to-t from-black/70  to-transparent
           p-3 text-white font-bold"
-          >
-            {game.name}
-          </p>
-        </figure>
-        {/* 8 empty divs needed for the 3D effect */}
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+            >
+              {game.name}
+            </p>
+          </figure>
+        </Link>
       </div>
     </>
   );
